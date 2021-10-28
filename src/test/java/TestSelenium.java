eac6a1b30a286ef3656653510b99070f0cf7cae7
Kirillova Olesya <olesyak-85@mail.ru>
2021-10-29 00:43:10 +0300

    added test to selenium

--- src/test/java/TestSelenium.java
+++ src/test/java/TestSelenium.java
@@ -15,11 +15,12 @@ public class TestSelenium {
     private WebDriver driver;
 
     @BeforeAll
-    static void beforeAll() {System.setProperty("webdriver.chrome.driver", "./driver/chromedriver/chromedriver.exe");
+    static void beforeAll() {
+        System.setProperty("webdriver.chrome.driver", "./driver/chromedriver/chromedriver.exe");
     }
 
     @BeforeEach
-    void setup(){
+    void setup() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
@@ -29,13 +30,13 @@ public class TestSelenium {
     }
 
     @AfterEach
-    void tearDown(){
+    void tearDown() {
         driver.quit();
         driver = null;
     }
 
     @Test
-    void shouldSendValidValue(){
+    void shouldSendValidValue() {
         WebElement form = driver.findElement(By.cssSelector("[action]"));
         driver.findElement(cssSelector("[data-test-id=name] input")).sendKeys("Петров Петр");
         driver.findElement(cssSelector("[data-test-id=phone] input")).sendKeys("+79999999999");
@@ -47,7 +48,7 @@ public class TestSelenium {
     }
 
     @Test
-    void shouldSendValidValueDoubleSurname(){
+    void shouldSendValidValueDoubleSurname() {
         WebElement form = driver.findElement(By.cssSelector("[action]"));
         driver.findElement(cssSelector("[data-test-id=name] input")).sendKeys("Петров-Сидоров Петр");
         driver.findElement(cssSelector("[data-test-id=phone] input")).sendKeys("+79999999999");
@@ -55,6 +56,30 @@ public class TestSelenium {
         driver.findElement(cssSelector("[role=button]")).click();
         String actualText = driver.findElement(cssSelector("[data-test-id=order-success]")).getText();
         String expectedText = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        Assertions.assertEquals(expectedText, actualText.trim());
+    }
+
+    @Test
+    void shouldSendInvalidValueName() {
+        WebElement form = driver.findElement(By.cssSelector("[action]"));
+        driver.findElement(cssSelector("[data-test-id=name] input")).sendKeys("Petr Petrov");
+        driver.findElement(cssSelector("[data-test-id=phone] input")).sendKeys("+79999999999");
+        driver.findElement(cssSelector("[data-test-id=agreement] .checkbox__box")).click();
+        driver.findElement(cssSelector("[role=button]")).click();
+        String actualText = driver.findElement(cssSelector("[data-test-id=name] .input__sub")).getText();
+        String expectedText = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
+        Assertions.assertEquals(expectedText, actualText.trim());
+    }
+
+    @Test
+    void shouldSendValueWithOutTelNumber() {
+        WebElement form = driver.findElement(By.cssSelector("[action]"));
+        driver.findElement(cssSelector("[data-test-id=name] input")).sendKeys("Петя Иванов");
+        driver.findElement(cssSelector("[data-test-id=phone] input")).sendKeys("");
+        driver.findElement(cssSelector("[data-test-id=agreement] .checkbox__box")).click();
+        driver.findElement(cssSelector("[role=button]")).click();
+        String actualText = driver.findElement(cssSelector(".input_invalid .input__sub")).getText();
+        String expectedText = "Поле обязательно для заполнения";
         Assertions.assertEquals(expectedText, actualText.trim());
     }
 
